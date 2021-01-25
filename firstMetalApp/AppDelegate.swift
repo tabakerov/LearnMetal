89771f489636370ca30ab4a290d702720c1e662d
Dmitry Tabakerov <tabakerov@outlook.com>
2021-01-25 15:22:09 +0300

    Initial Commit

---
+++ firstMetalApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  firstMetalApp
+//
+//  Created by Dima on 25/01/2021.
+//  Copyright © 2021 Dima. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ firstMetalApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  firstMetalApp
+//
+//  Created by Dima on 25/01/2021.
+//  Copyright © 2021 Dima. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
